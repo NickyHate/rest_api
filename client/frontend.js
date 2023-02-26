c553cb1394413f7c666c0376eb5deee223c4eba6
Nikita <nikitostokarev22@gmail.com>
2023-02-26 22:19:52 +0300

    create client side

--- client/frontend.js
+++ client/frontend.js
@@ -1,3 +1,41 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.8/dist/vue.esm.browser.js';
 
-console.log(Vue)
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [
+                {
+                    id:1,
+                    name: 'Nikita',
+                    value: '+7-911-72-72-728',
+                    marked: false
+                }
+            ]
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.value && this.form.name
+        }
+    },
+    methods: {
+        createContact() {
+            const {...contact} = this.form
+            this.contacts.push({...contact, id: Date.now(), marked: false})
+            this.form.name = ''
+            this.form.value = ''
+        },
+        markContact(id) {
+            const contact = this.contacts.find(c => c.id === id)
+            contact.marked = true
+        },
+        removeContact(id) {
+            this.contacts = this.contacts.filter(c => c.id !== id)
+        }
+    }
+})
